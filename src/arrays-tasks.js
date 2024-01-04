8614cea684c3c1f6abf8aca5c0c0cbd34d67f601
Vitali Kuntsevich <kvadesstudio@gmail.com>
2024-01-04 16:31:17 +0300

    feat: add getAverage, isSame, isValue

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -124,8 +124,13 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const sum = arr.reduce((acc, val) => acc + val, 0);
+  const average = sum / arr.length;
+  return Math.round(average * 100) / 100; // Rounding to two decimal places
 }
 
 /**
@@ -138,8 +143,8 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((str, index, array) => str.length === array[0].length);
 }
 
 /**
@@ -153,8 +158,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((value, index) => value === index);
 }
 
 /**
