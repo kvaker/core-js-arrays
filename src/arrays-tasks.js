063084f250de60163a6d18c0256b44d64dd01d1c
Vitali Kuntsevich <kvadesstudio@gmail.com>
2024-01-05 16:48:46 +0300

    feat: add insertItem, getHead, getTail, toString, distinct

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -173,8 +173,9 @@ function isValueEqualsIndex(arr) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  Array.prototype.splice.apply(arr, [index, 0].concat(item));
+  return arr;
 }
 
 /**
@@ -188,8 +189,9 @@ function insertItem(/* arr, item, index */) {
  *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
  *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  const slicedList = arr.slice(arr, n);
+  return slicedList;
 }
 
 /**
@@ -203,8 +205,9 @@ function getHead(/* arr, n */) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  const lastRight = arr.slice(arr.length - n, arr.length);
+  return lastRight;
 }
 
 /**
@@ -234,8 +237,9 @@ function doubleArray(/* arr */) {
  *    toStringList([1, 2, 3, 4, 5]) => '1,2,3,4,5'
  *    toStringList(['rock', 'paper', 'scissors']) => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  const str = arr.join(',');
+  return str;
 }
 
 /**
@@ -250,8 +254,9 @@ function toStringList(/* arr */) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const unique = Array.from(new Set(arr));
+  return unique;
 }
 
 /**
